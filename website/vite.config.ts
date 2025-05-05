1fe4b61135ef2cf577c3f14746cc84b1adfc6911
Yueming Hao <yhao@meta.com>
2025-05-05 14:09:41 -0700

    add website configs (#3)
    Summary:
    This PR starts adding website part for tritonparse
    - Add .gitignore file
    - Add website package dependencies and configs
    
    Pull Request resolved: https://github.com/pytorch-labs/tritonparse/pull/3
    
    Reviewed By: davidberard98
    
    Differential Revision: D74186565
    
    Pulled By: FindHao
    
    fbshipit-source-id: a9150df508d42b014b3b90cba0b831ddba7df526

---
+++ website/vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
+
+const packageJson = JSON.parse(
+  readFileSync(resolve(__dirname, 'package.json'), 'utf-8')
+)
+
+export default defineConfig({
+  plugins: [
+    react(),
+    {
+      name: 'strip-module-attr',
+      enforce: 'post',
+      transformIndexHtml(html) {
+        return html.replace(
+          /<script\s+type=["']module["']\s+([^>]*?)src=/g,
+          '<script defer $1src='
+        )
+      }
+    }
+  ],
+
+  base: './',
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        format: 'iife',
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  },
+  define: {
+    'import.meta.env.PACKAGE_VERSION': JSON.stringify(packageJson.version)
+  }
+})
