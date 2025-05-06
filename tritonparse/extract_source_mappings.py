ffc7f8877cfa9ffc88073805ee34a775a0bc63a5
Yueming Hao <yhao@meta.com>
2025-05-06 16:08:58 -0700

    Split of "[TritonParse]Add entrace of source code mapping script"
    Summary: reorder the whole stack
    
    Reviewed By: davidberard98
    
    Differential Revision: D74278075
    
    fbshipit-source-id: 2ce46d69babdc9fe8302d44e55fd83354f79a298

---
+++ tritonparse/extract_source_mappings.py
@@ -0,0 +1,11 @@
+"""
+Extract source code mappings from Triton trace files and update the original JSON.
+This script reads a JSON trace file containing Triton IR (TTIR, TTGIR) and PTX(AMDGCN),
+and extracts bidirectional mappings between:
+- Python ↔ TTIR
+- Python ↔ TTGIR
+- Python ↔ PTX(AMDGCN)
+- TTIR ↔ TTGIR
+- TTIR ↔ PTX(AMDGCN)
+- TTGIR ↔ PTX(AMDGCN)
+"""
