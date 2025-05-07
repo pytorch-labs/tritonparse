83c5e23c0d65ad586cc3843a779b8b53785f1c21
Yueming Hao <yhao@meta.com>
2025-05-06 19:21:03 -0700

    add create_python_mapping
    Summary: This diff adds argparse to the TritonParse project. The code changes include adding the argparse library to the project and modifying the existing code to use argparse for command line argument parsing. The get_file_extension function is also added to extract the file extension from a given filename. The create_python_mapping function is modified to accept a list of tuples containing the IR maps and returns a dictionary of the mapping.
    
    Reviewed By: davidberard98
    
    Differential Revision: D74284154
    
    fbshipit-source-id: a7b5279d4f85387317cea2c138b5b3e9bb2a32ac

--- tritonparse/extract_source_mappings.py
+++ tritonparse/extract_source_mappings.py
@@ -10,3 +10,60 @@ and extracts bidirectional mappings between:
 - TTIR ↔ PTX(AMDGCN)
 - TTGIR ↔ PTX(AMDGCN)
 """
+
+import argparse
+from collections import defaultdict
+from typing import Any, Dict, List, Tuple
+
+
+def get_file_extension(filename: str) -> str:
+    """
+    Get the file extension from a given filename or return the filename itself if it has no extension.
+
+    Args:
+        filename (str): The filename or file extension.
+
+    Returns:
+        str: The file extension or the filename itself if no extension is present.
+    """
+    # Split the filename by '.' and return the last part if it exists
+    parts = filename.split(".")
+    return parts[-1] if len(parts) > 1 else filename
+
+
+def create_python_mapping(
+    ir_maps: List[Tuple[str, Dict[str, Dict[str, Any]]]],
+) -> Dict[int, Dict[str, List[int]]]:
+    """
+    Create a mapping from Python source code to IR mappings. We assume there is only one Python source code for each triton kernel.
+    Args:
+        ir_maps: A list of tuples containing the IR type and the IR mappings.
+
+    Returns:
+        A dictionary mapping Python source code line numbers to their corresponding IR mappings.
+    """
+    py_map = defaultdict(defaultdict(list))
+    for ir_type, ir_map in ir_maps:
+        for line_number, info in ir_map.items():
+            py_line_number: int = info["line"]
+            py_map[py_line_number][f"{ir_type}_lines"].append(line_number)
+    return py_map
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Extract source code mappings from Triton trace files."
+    )
+    parser.add_argument("-i", "--input", help="Path to the Triton trace NDJSON file")
+    parser.add_argument(
+        "--output-dir",
+        default=None,
+        help="Directory to save the output files. If not specified, the input file's directory will be used.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        default=None,
+        help="Output NDJSON path. If it is None, the default output file name will be set to {input}_mapped.ndjson in the parse function.",
+    )
+    return parser.parse_args()
